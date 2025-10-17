ab7021a823065d4a89a11e2e29d520ac84a85ecf
sgf145872-bit <sgf145872@gmail.com>
2025-10-17 18:15:17 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // طريقة 2: استخدام كائن
 class Response {
     public $message = "serveron";
@@ -6,11 +7,12 @@ class Response {
 
 $response = new Response();
 echo json_encode($response);
-
+*/
 // طريقة 3: مع إضافة رأس JSON
 header('Content-Type: application/json');
 echo json_encode(["status" => "serveron"]);
-
+/*
 // طريقة 4: مع خيارات إضافية
 echo json_encode(["response" => "serveron"], JSON_PRETTY_PRINT);
+*/
 ?>
