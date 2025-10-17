8ba6b5375b681f8b1a7ed654d25755cb09ccb42f
sgf145872-bit <sgf145872@gmail.com>
2025-10-17 18:14:29 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,8 +1,16 @@
 <?php
-// طريقة 1: استخدام مصفوفة منتظمة
-$data = array("message" => "serveron");
-echo json_encode($data);
+// طريقة 2: استخدام كائن
+class Response {
+    public $message = "serveron";
+}
 
-// أو يمكنك كتابتها بطريقة أقصر:
-// echo json_encode(["message" => "serveron"]);
+$response = new Response();
+echo json_encode($response);
+
+// طريقة 3: مع إضافة رأس JSON
+header('Content-Type: application/json');
+echo json_encode(["status" => "serveron"]);
+
+// طريقة 4: مع خيارات إضافية
+echo json_encode(["response" => "serveron"], JSON_PRETTY_PRINT);
 ?>
