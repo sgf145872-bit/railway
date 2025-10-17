ce620a8aed9b70f21454df70d69222897c23c193
sgf145872-bit <sgf145872@gmail.com>
2025-10-17 18:07:58 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php
+// طريقة 1: استخدام مصفوفة منتظمة
+$data = array("message" => "serveron");
+echo json_encode($data);
+
+// أو يمكنك كتابتها بطريقة أقصر:
+// echo json_encode(["message" => "serveron"]);
+?>
